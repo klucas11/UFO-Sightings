59a6891fa4a93794e715c10d566220fd7e944022
Kristin Lucas <klucas11@aggienetwork.com>
2019-08-19 21:50:56 -0500

    Filters by datetime

--- static/js/app.js
+++ static/js/app.js
@@ -1,28 +1,46 @@
 // from data.js
 var tableData = data;
 
-// Appends table to webpage
-var tbody = d3.select("tbody");
-console.log(tableData);
 
-data.forEach(function (sightingsData) {
-    var row = tbody.append("tr");
-    Object.entries(sightingsData).forEach(function ([key, value]) {
-        console.log(key, value);
-        var cell = row.append("td");
-        cell.text(value);
+// selecting input 
+var submit = d3.select("#filter-btn");
+
+submit.on("click", function () {
+    d3.event.preventDefault();
+    var inputElement = d3.select("#datetime");
+    var inputValue = inputElement.property("value");
+
+    console.log(inputValue)
+
+    // Filters Data
+    var filteredData = tableData.filter(sighting => sighting.datetime === inputValue);
+
+    console.log(filteredData);
+
+    // Appends table to webpage
+    var tbody = d3.select("tbody");
+
+    filteredData.forEach(function (sightingsData) {
+        var row = tbody.append("tr");
+        Object.entries(sightingsData).forEach(function ([key, value]) {
+            var cell = row.append("td");
+            cell.text(value);
+        });
     });
 });
 
+
+
+
 // Creates Filters
-var filterUFOSightins = function (sightings, targetColumn, filterValue) {
-    /*
-        Purpose: Return a filtered list of sightings using targetColum and filterValue.
-        sightings: Array of objects
-        targetColumn: String, key of sighting object to filter by
-        filterValue: Mixed, value used to find matches on targetColumns
-    */
-    let filteredList = [];
+// var filterUFOSightins = function (sightings, targetColumn, filterValue) {
+/*
+Purpose: Return a filtered list of sightings using targetColum and filterValue.
+sightings: Array of objects
+targetColumn: String, key of sighting object to filter by
+filterValue: Mixed, value used to find matches on targetColumns
+*/
+    // let filteredList = [];
     // // loop each sighting object in sightings
     // for (let sightingIndex = 0; sightingIndex < sightings.length; sightingIndex++) {
     //     // let sightingObject = sightings[sightingIndex];
@@ -32,14 +50,15 @@ var filterUFOSightins = function (sightings, targetColumn, filterValue) {
     //         // // // append sighting object to filteredList
     //     }
     // }
-    sightings.forEach(function (sightingObject) {
-        if (sightingObject[targetColumn] === filterValue) {
-            filteredList.push(sightingObject);
-        }
-    });
+    // sightings.forEach(function (sightingObject) {
+    //     if (sightingObject[targetColumn] === filterValue) {
+    //         filteredList.push(sightingObject);
+    //     }
+    // });
 
     // return filteredList;
+    // console.log(filteredList);
 
-    // return sightings.filter(sightingObject => sightingObject[targetColumn] === filterValue)
-}
-filterUFOSightins(data, 'city', 'benton')
+    //         // return sightings.filter(sightingObject => sightingObject[targetColumn] === filterValue)
+    //     }
+    // filterUFOSightins(data, 'city', 'benton');
